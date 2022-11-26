9483e445881a8fcf3f08945f811fbc96922c4f6b
Muralitharan171 <118879071+Muralitharan171@users.noreply.github.com>
2022-11-27 00:38:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// 1.How to compare two JSON have the same properties without order?
+
+var obj1={name:"person 1", age:"5"};
+var obj2={age:"5", name:"person 1"};
+
+if(Object.keys(obj1).length===Object.keys(obj2).length){
+    for (var key in obj1)
+       if(obj1[key] == obj2[key]) {
+          }// console.log("equal");
+       }
+       else {
+          // console.log("not equal");  
+}
+
+//2.use the rest countries API URL "https://restcountries.eu/rest/v3/all" and display all the country flags in the console
+
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all",true)
+request.send()
+ request.onload=function(){
+    var data = request.response
+    var result = JSON.parse(data)
+    for(var obj of result){
+    //   console.log( obj.flag);
+    }
+    }
+
+//3.use same rest countries and print all countries names,regions,sub regions and populations
+
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all",true)
+request.send()
+ request.onload=function(){
+    var data = request.response
+    var result = JSON.parse(data)
+    for(var obj of result){
+       console.log( obj.name.common,obj.region,obj.subregion,obj.population);
+    }
+    }
